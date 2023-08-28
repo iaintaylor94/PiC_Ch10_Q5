3739190755f07b30539b3d516cafb0b11865ac55
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-28 12:53:08 +0000

    fixed errors in findString()

--- .ccls-cache/@home@runner@Ch10Q5-find-string/main.c
+++ .ccls-cache/@home@runner@Ch10Q5-find-string/main.c
@@ -11,17 +11,32 @@ int main(void) {
   const char string1[] = "one two three four";
   const char string2[] = "three";
   const char string3[] = "seven";
+  const char string4[] = "one";
+  const char string5[] = "two";
+  const char string6[] = "four";
 
   // Print string1
   printf ("string: %s\n", string1);
 
   // Find string2
   int location = findString (string2, string1);
-  printf ("%s: %d\n", string2, location);
+  printf ("%s: %d\n\n", string2, location);
 
   // Find string3
   location = findString (string3, string1);
-  printf ("%s: %d\n", string3, location);
+  printf ("%s: %d\n\n", string3, location);
+
+  // Find string4
+  location = findString (string4, string1);
+  printf ("%s: %d\n\n", string4, location);
+
+  // Find string5
+  location = findString (string5, string1);
+  printf ("%s: %d\n\n", string5, location);
+
+  // Find string5
+  location = findString (string6, string1);
+  printf ("%s: %d\n\n", string6, location);
   
   return 0;
 }
@@ -29,13 +44,19 @@ int main(void) {
 // Define Functions
 int findString (const char find[], const char within[]) {
   int location = -1;
-  
-  for (int i = 0; i < strlen(within); i++)
-    for (int j = 0; j < strlen(find) || i + j < strlen(within); j++) {
+  int i, j;
+
+  printf ("%s\n", find);
+  for (i = 0; i < strlen(within); i++) {
+    printf ("%2d: ", i);
+    for (j = 0; j < strlen(find); j++) {
+      printf ("%c", within[i + j]);
       if (find[j] != within[i + j])
         break;
-      location = i;
     }
-  
+    if (j == strlen(find))
+      location = i;
+    printf ("\n");
+  }
   return location;
 }
